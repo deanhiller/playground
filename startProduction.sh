d267744c2a833f8e518cf1dbdfee19c499b252b7
Dean Hiller <dean@alvazan.com>
2013-05-23 08:39:39 -0600

    fix for production use

--- startProduction.sh
+++ startProduction.sh
@@ -2,5 +2,10 @@
 
 #NOTE: Developers, PLEASE do not start this script and start using the 
 #production database!!!!
-play-1.2.5/play run --%prod -Xmx1024M
+
+cp conf/prod.logback.xml conf/logback.xml
+
+play-1.2.5/play run --%prod -Xmx1024M &
+disown
+
 #-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1044
